56084fb7591d907723f1e0788b2b202f450b5d04
mukku <>
2021-04-23 10:50:11 +0900

    DAY1

---
+++ FizzBuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(number)
+  if number % 15 == 0
+    "fizzbuzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else
+    number.to_s
+  end
+end
+
+a = gets.to_i
+
+puts fizzbuzz(a)
